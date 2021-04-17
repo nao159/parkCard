34c788f917504fdb631214c37840de0f0d4783ce
Maxim Vainikka <rithmique@yahoo.com>
2021-04-17 16:41:51 +1000

    Initial Commit

---
+++ rememberCard/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  rememberCard
+//
+//  Created by Максим Нуждин on 17.04.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ rememberCard/rememberCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  rememberCardApp.swift
+//  rememberCard
+//
+//  Created by Максим Нуждин on 17.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct rememberCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
